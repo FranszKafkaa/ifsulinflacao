532ec9cc6fd15c360b919acc55dc9e20868fe44a
Marcelo <marcelosjanke@icloud.com>
2022-07-15 23:03:59 -0300

    load CPI file data into array

--- src/inflacao.c
+++ src/inflacao.c
@@ -1,5 +1,33 @@
 #include "inflacao.h"
 
-void show_text(const char *bolo) {
-    printf("%s\n", bolo);
+float maxData[2023];
+
+
+void readfile() {
+    FILE *fp;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    fp = fopen("data.txt", "r");
+    if (fp == NULL)
+        exit(0);
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+        char *token = strtok(line, ";");
+        int ano = atoi(token);
+
+        token = strtok(NULL, ";");
+        float valor = atof(token);
+
+        maxData[ano] = valor;
+
+    }
+
+
+    printf("%f\n", maxData[2012]);
+    printf("\n");
+    fclose(fp);
+    if (line)
+        free(line);
 }
